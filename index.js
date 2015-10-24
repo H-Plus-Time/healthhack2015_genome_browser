c472725998195ed3c9bf28db157433f23848fc20
Nicholas Roberts <Nicholas.Roberts.au@gmail.com>
2015-10-24 13:55:00 +1100

    Fetch genomes at page load

--- index.js
+++ index.js
@@ -11,44 +11,44 @@ connection.onopen = function(session) {
 //  uploadRa = document.getElementById('uploadRa');
     
   console.log("Connected", session.details);
-//  session.subscribe('com.example.upload.on_progress', function(args) {
-//    var pinfo = args[0];
-//    console.log('upload event received', pinfo.status, pinfo.chunk, pinfo.remaining, pinfo.total, pinfo.progress);
-//  });
-//
-//  var r = new Resumable({
-//    target: 'upload',
-//    chunkSize: 1 * 1024 * 1024,
-//    forceChunkSize: true, // https://github.com/23/resumable.js/issues/51
-//    simultaneousUploads: 4,
-//    testChunks: true,
-//    query: {
-//      on_progress: 'com.example.upload.on_progress',
-//      session: session.id
-//    }
-//  });
-//  if (!r.support) {
-//    console.log("Fatal: ResumableJS not supported!");
-//  } else {
-//    r.assignBrowse([uploadFasta, uploadBED, uploadRa]);
-//    r.on('fileAdded', function(file) {
-//      console.log('fileAdded', file);
-//      r.upload();
-//    });
-//    r.on('fileSuccess', function(file, message) {
-//      console.log('fileSuccess', file, message);
-//      console.log(r.files);
-//      // enable repeated upload since other user can delete the file on the server
-//      // and this user might want to reupload the file
-//      file.cancel();
-//    });
-//    r.on('fileError', function(file, message) {
-//      console.log('fileError', file, message);
-//    });
-//  }
-//
-//  upload_reference_genome = function() {}
-//  
+  session.subscribe('com.example.upload.on_progress', function(args) {
+    var pinfo = args[0];
+    console.log('upload event received', pinfo.status, pinfo.chunk, pinfo.remaining, pinfo.total, pinfo.progress);
+  });
+
+  var r = new Resumable({
+    target: 'upload',
+    chunkSize: 1 * 1024 * 1024,
+    forceChunkSize: true, // https://github.com/23/resumable.js/issues/51
+    simultaneousUploads: 4,
+    testChunks: true,
+    query: {
+      on_progress: 'com.example.upload.on_progress',
+      session: session.id
+    }
+  });
+  if (!r.support) {
+    console.log("Fatal: ResumableJS not supported!");
+  } else {
+    r.assignBrowse([uploadFasta, uploadBED, uploadRa]);
+    r.on('fileAdded', function(file) {
+      console.log('fileAdded', file);
+      r.upload();
+    });
+    r.on('fileSuccess', function(file, message) {
+      console.log('fileSuccess', file, message);
+      console.log(r.files);
+      // enable repeated upload since other user can delete the file on the server
+      // and this user might want to reupload the file
+      file.cancel();
+    });
+    r.on('fileError', function(file, message) {
+      console.log('fileError', file, message);
+    });
+  }
+
+  upload_reference_genome = function() {}
+  
   session.call('com.gb.taxon_search', ['drosophila birchii']).then(
       function(res) {
           console.log(res);
@@ -66,8 +66,20 @@ document.addEventListener('WebComponentsReady', function() {
   connection.open();
   template.selected = 0;
   template.subselected = 0;
-//  document.getElementById('bedNavButton').addEventListener('click', function(e) {
-//    template.subselected = 1;
-//  });
+  document.getElementById('bedNavButton').addEventListener('click', function(e) {
+    template.subselected = 1;
+  });
     console.log("Opened connection");
+  genomeGrid = document.getElementById('genomeGrid');
+  connection.session.call('com.gb.fetch_genomes').then(
+      function(res) {
+          template.genomes = res;
+          genomeGrid.items = template.genomes;
+          genomeGrid.clearCache();
+          console.log(res);
+      },
+      function(err) {
+          console.log(err);
+      }
+  );
 });
